234c019ea1ddea5ba99e07724d7c283db1ea34c8
EdPaul1 <saidarajab101@gmail.com>
2023-03-10 08:06:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here
+main.remove();
+const newHeader = document.createElement("h1");
+// document.newHeader.append(id='victory');
+// // h1.textContent = "Edwin is the champion";
+// newHeader.id = "vicotry";
+// newHeader = document.querySelector('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Edwin is the champion";
